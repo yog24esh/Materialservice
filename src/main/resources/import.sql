6b097c5593bba50835ea508a5d94712cc327a3c3
Khozema Nullwala <khozema.nullwala@accenture.com>
2019-08-07 12:07:59 +0530

    Updated import.sql based on request received on 07th August 2019

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,6 @@
 INSERT INTO material_category (category_id, category_name) VALUES ('C001', 'Thread');
 INSERT INTO material_category (category_id, category_name) VALUES ('C002', 'Cloth');
 INSERT INTO material_category (category_id, category_name) VALUES ('C003', 'Button');
+
+-- Please build further queries to add
+-- 'MaterialType' and 'Unit' details of this service below.
